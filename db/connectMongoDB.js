5e1ce0d7b04c5139ad960f52d2fdd2ca39f44104
AhmdBlack0 <AhmdBlack.0@gmail.com>
2025-11-15 20:09:45 +0200

    updates

--- db/connectMongoDB.js
+++ db/connectMongoDB.js
@@ -3,10 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("MongoDB connected successfully");
   } catch (err) {
     console.error("MongoDB connection error:", err);
